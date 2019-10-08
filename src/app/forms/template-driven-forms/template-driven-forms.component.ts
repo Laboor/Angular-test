7b2ef4676001dbc0651328c2815250eb1ab22879
Avalon <972003502@qq.com>
2019-10-09 00:35:12 +0800

    '2019.10.9'

--- src/app/forms/template-driven-forms/template-driven-forms.component.ts
+++ src/app/forms/template-driven-forms/template-driven-forms.component.ts
@@ -17,9 +17,12 @@ export class TemplateDrivenFormsComponent implements OnInit {
 
   onSubmit() { this.submitted = true; }
 
-  // TODO: Remove this when we're done
-  get diagnostic() { return JSON.stringify(this.model); }
-
+  newHero() {
+    this.model = new Hero(42, '', '');
+  }
+  onClick(obj: any) {
+    console.log(obj);
+  }
   ngOnInit() {
   }
 }
