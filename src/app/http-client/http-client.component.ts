b02cdb8dfdafb6706c5d21be6d9b61175777b4cb
Avalon <972003502@qq.com>
2019-11-01 00:17:22 +0800

    '2019.11.1'

--- src/app/http-client/http-client.component.ts
+++ src/app/http-client/http-client.component.ts
@@ -19,6 +19,70 @@ export class HttpClientComponent implements OnInit {
   heroes: MyHero[];
   hero: MyHero;
 
+  test() {
+    // fetch('http://localhost:3000/heroes/api/?model=hero', {
+    //   method: 'GET',
+    //   headers: {
+    //     Accept: 'text/plain',
+    //     'Accept-Language': 'zh-CN',
+    //     'Content-Language': 'zh-CN',
+    //     'Content-Type': 'text/plain',
+    //     'Access-Control-Request-Method': 'DELETE, PUT',
+    //     'Access-Control-Request-Headers': 'Content-Type'
+    //   },
+    //   body: undefined,
+    //   referrer: 'http://localhost:4200/http/custom',
+    //   referrerPolicy: 'no-referrer-when-downgrade',
+    //   mode: 'cors',
+    //   keepalive: true
+    // }).then(response => response.json())
+    //   .then(res => console.log(res));
+
+    const hero1 = {
+      id: '5',
+      name: 'john'
+    };
+    const sayHi = 'Hello World';
+
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', 'http://localhost:3000/heroes');
+    xhr.timeout = 10000; // 设置连接超时，超时时触发ontimeout事件
+    xhr.responseType = 'json'; // 响应的数据解析成json格式
+
+    xhr.setRequestHeader('Accept', 'text/plain');
+    xhr.setRequestHeader('Accept-Language', 'zh-CN');
+    xhr.setRequestHeader('Content-Language', 'zh-CN');
+    xhr.setRequestHeader('Content-Type', 'text/plain');
+    xhr.send(sayHi); // 发送数据
+    // xhr.abort();
+
+    // 接收到响应
+    xhr.onload = () => {
+      console.log(xhr.getAllResponseHeaders());
+      console.log(xhr.status, xhr.statusText, xhr.response);
+    };
+    // 触发错误
+    xhr.onerror = () => {
+      console.log(new Error('error'));
+    };
+    // 进度监听
+    xhr.onprogress = (event) => {
+      console.log(`Received ${event.loaded} of ${event.total}`);
+    };
+    // 超时
+    xhr.ontimeout = () => {
+      console.log(`timeout ${xhr.timeout}`);
+    };
+    // 状态变化监听
+    xhr.onreadystatechange = () => {
+      console.log('state:', xhr.readyState);
+    };
+    // 中止请求监听
+    xhr.onabort = () => {
+      console.log('请求中止！');
+    };
+  }
+
   clickGetHeroes() {
     this.configService.getHeroes()
       .subscribe(
